652219c1b5c21c4a837e43e0539cbb4274ca193c
therangerangler <64225303+therangerangler@users.noreply.github.com>
2026-05-12 10:14:32 -0700

    Update external-links.ts

--- src/gui/src/app/constants/external-links.ts
+++ src/gui/src/app/constants/external-links.ts
@@ -1,8 +1,4 @@
-export const SUPPORT_PAGE_URL = 'https://docs.aws.amazon.com/nimble-studio/latest/filetransfer-guide/support.html';
-
-export const MARKETING_PAGE_URL = 'https://aws.amazon.com/nimble-studio/features/file-transfer-client';
-
 export const docsLinks = {
-    GETTING_STARTED_PAGE_URL: 'https://docs.aws.amazon.com/nimble-studio/latest/filetransfer-guide/getting-started.html',
-    USER_GUIDE_PAGE_URL: 'https://docs.aws.amazon.com/nimble-studio/latest/filetransfer-guide/what-is-file-transfer.html',
+    GETTING_STARTED_PAGE_URL: 'https://github.com/awslabs/filemoverexpress/blob/main/docs/Getting-Started.md',
+    USER_GUIDE_PAGE_URL: 'https://github.com/awslabs/filemoverexpress/blob/main/docs/Quick-Start.md',
 };
